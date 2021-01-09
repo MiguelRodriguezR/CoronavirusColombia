21668c96e02bbf375e49b321a1db25a0bc00b129
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2021-01-08 19:16:29 -0500

    queries repaired

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,10 +1,11 @@
 export const OPEN_DATA_SERVER = 'https://www.datos.gov.co/resource/gt2j-8ykr.json';
 export const QUERIES = [
-  'SELECT departamento, count(atenci_n) as infected GROUP BY departamento', //Contagiados
-  'SELECT departamento, count(atenci_n) as deaths WHERE estado = "Fallecido" GROUP BY departamento', //Fallecidos
-  'SELECT departamento, count(atenci_n) as imported WHERE tipo = "Importado" GROUP BY departamento', //Importados
-  'SELECT departamento, count(atenci_n) as related WHERE tipo = "Relacionado" GROUP BY departamento', //Relacionados
-  'SELECT departamento, count(atenci_n) as onStudy WHERE tipo = "En estudio" GROUP BY departamento', //En Estudio
-  'SELECT departamento, max(edad) as olderInfected, min(edad) as youngerInfected GROUP BY departamento', //Edad Mayor, Menor Contagiado
-  'SELECT departamento, max(edad) as olderDeath, min(edad) as youngerDeath WHERE estado = "Fallecido" GROUP BY departamento', //Edad Mayor, Menor Muerto
+  'SELECT departamento_nom, count(edad) as infected GROUP BY departamento_nom', //Contagiados
+  'SELECT departamento_nom, count(edad) as deaths WHERE estado = "Fallecido" GROUP BY departamento_nom', //Fallecidos
+  'SELECT departamento_nom, count(edad) as imported WHERE fuente_tipo_contagio = "Importado" GROUP BY departamento_nom', //Importados
+  'SELECT departamento_nom, count(edad) as related WHERE fuente_tipo_contagio = "Relacionado" GROUP BY departamento_nom', //Relacionados
+  'SELECT departamento_nom, count(edad) as onStudy WHERE fuente_tipo_contagio = "En estudio" GROUP BY departamento_nom', //En Estudio
+  'SELECT departamento_nom, max(edad) as olderInfected, min(edad) as youngerInfected GROUP BY departamento_nom', //Edad Mayor, Menor Contagiado
+  'SELECT departamento_nom, max(edad) as olderDeath, min(edad) as youngerDeath WHERE estado = "Fallecido" GROUP BY departamento_nom', //Edad Mayor, Menor Muerto
 ];
+
